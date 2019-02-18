ea524f716eb76fb135a7a9e05bd74b69db12d7af
Lu Xie <xiemegan447@gmail.com>
2019-02-18 16:56:34 -0500

    adding separation of concerns and renaming the App.vue component for kebab-case

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.component.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
