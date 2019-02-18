d3a4e84f6e2a12427eaaf62e7881581efc89d401
Lu Xie <xiemegan447@gmail.com>
2019-02-18 17:32:51 -0500

    creating side-navigation component and wiring up

--- src/app.component.vue
+++ src/app.component.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-aside width="200px">
+        <side-navigation/>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
 
 <script src="./app.component.js"></script>
-
-
 <style src="./app.component.css"></style>
