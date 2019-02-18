0947162020e894bde3f35c5a9d3fa04dc6d90687
Lu Xie <xiemegan447@gmail.com>
2019-02-18 18:07:28 -0500

    completing router-implementation

---
+++ src/components/pages/dashboard-page/dashboard-page.component.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="dashboardPage">Dashboard Page</div>
+</template>
+<script src="./dashboard-page.component.js"></script>
+<style src="./dashboard-page.component.css"></style>
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './app.component.vue'
 import VueRouter from 'vue-router'
+import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -10,5 +11,6 @@ Vue.use(VueRouter)
 Vue.use(ElementUI)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
